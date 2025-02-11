360b2488bc9285bc06c313fbcac5e0bde7883ddd
Samarthgit0012 <samarthdec12@gmail.com>
2025-02-11 16:26:09 +0530

    initial

---
+++ Day1/first.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class first
+{
+    static {
+        System.out.println("Hello World");
+        System.exit(0);
+    }
+    public  static void main (String [] args)
+    {
+
+    }
+}
