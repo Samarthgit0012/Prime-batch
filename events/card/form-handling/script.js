a701db7c80c440ef28c2119189271eacd7d0b498
Samarthgit0012 <samarthdec12@gmail.com>
2025-02-20 16:49:07 +0530

    form-handling

---
+++ events/card/form-handling/script.js
@@ -0,0 +1,12 @@
+const handlleTextChange =(e)=> {
+    console.log(e);
+    console.log("Change : ", e.target.value);
+};
+const handleKeyDown =(e)=> {
+    console.log(e);
+    console.log("Key Down : ", e.target.value);
+};
+const handleKeyUp = (e) => {
+    console.log(e);
+    console.log("Key Up : ", e.target.value);
+}
